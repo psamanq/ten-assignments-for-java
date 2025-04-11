db9faf261ee05ffaa1a8fb9b136b03a433bfe495
psamanq <persiking123@gmail.com>
2025-04-11 20:32:54 +0330

    practice 3

--- Assignment3/main/src/App.java
+++ Assignment3/main/src/App.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scanner = new Scanner(System.in);
+        String userText = scanner.nextLine();
+
+        boolean iscorrect = true;
+
+        String array = "";
+
+        for(int n = 0; n < userText.length(); n++){
+            
+            char ch = userText.charAt(n);
+
+            if ((ch == '(') || (ch == '{') ||  (ch == '[')){
+                array += ch;
+            } 
+
+            else if((ch == ')') ||  (ch == '}') || (ch == ']')){
+                if(array.isEmpty()) {
+                    iscorrect =  false;
+                }
+                char last = array.charAt(array.length() - 1);
+
+                if((ch == ')' && last == '(') || 
+                   (ch == '}' && last == '{') || 
+                   (ch == ']' && last == '[')){
+                    array = array.substring(0, array.length()-1);
+                }else{
+                    iscorrect =  false;
+
+                }
+            }
+        }
+
+        if(iscorrect){
+            System.out.print("its correct");
+
+        }else {
+            System.out.print("its not correct");
+            
+        } 
+
+        scanner.close();
     }
 }
